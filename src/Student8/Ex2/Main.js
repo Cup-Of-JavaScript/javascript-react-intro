c89bef73ac7cdf81499e936ddbeb59e782ce9587
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-06-14 13:55:15 -0400

    Ex2 Stinky FootBook

--- src/Student8/Ex2/Main.js
+++ src/Student8/Ex2/Main.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Header } from './Header'
+import { Footer } from './Footer'
+import { Posts } from './Posts'
+import { Groups } from './Groups'
 import './Main.css'
 
 export const Main = () => {
   return (
     <div>
-        Hello I'm student id: 8.
+        <Header />
+        <Posts />
+        <Groups />
+        <Footer />
     </div>
   )
 }
