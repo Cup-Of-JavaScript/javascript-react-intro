d7442581b76fec73dc9b52b4ff29f5e590a4c0a5
Kelsey Perkins <kperkins@xpanxion.com>
2022-06-14 10:57:14 -0700

    components

---
+++ src/Student7/Ex1/Dessert.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Dessert.scss'
+
+export const Dessert = () => {
+    return (
+
+        <div className='Dessert'>
+        <h3>Dessert</h3>
+        <hr/>
+            <ul>
+                <li>Chocolate Pie... $1.00</li>
+                <li>Apple Pie.. $1.00</li>
+                <li>Cannoli... $1.00</li>
+            </ul>
+        </div>
+    )
+}
--- src/Student7/Ex1/Menu.js
+++ src/Student7/Ex1/Menu.js
@@ -1,43 +1,17 @@
 import React from 'react'
 import './Menu.scss'
+import { Food } from './Food'
+import { Drink } from './Drink'
+import { Dessert } from './Dessert'
 
 export const Menu = () => {
   return (
     <div>
         <div className='Menu'>
            Menu
-           <div className='Food'>
-           <h3>Food</h3>
-           <hr/>
-           <ul>
-            <li>Hamburger... $1.00</li>
-            <li>Cheeseburger... $1.00</li>
-            <li>Hot Dog... $1.00</li>
-            <li>Fries... $1.00</li>
-           </ul>
-           </div>
-
-           <div className='Drink'>
-           <h3>Drink</h3>
-           <hr/>
-           <ul>
-            <li>Coffee... $1.00</li>
-            <li>Tea... $1.00</li>
-            <li>Milk... $1.00</li>
-            <li>Coke... $1.00</li>
-           </ul>
-           </div>
-
-           <div className='Dessert'>
-           <h3>Dessert</h3>
-           <hr/>
-           <ul>
-            <li>Chocolate Pie... $1.00</li>
-            <li>Apple Pie.. $1.00</li>
-            <li>Cannoli... $1.00</li>
-           </ul>
-           </div>
-
+            <Food />
+            <Drink />
+            <Dessert />
         </div>
     </div>
   )
