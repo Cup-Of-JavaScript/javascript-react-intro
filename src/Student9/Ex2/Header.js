c5dab1e2695d3f6f5450f69c868f2e3b4aefa105
steven davis <sdavis@xpanxion.com>
2022-06-14 12:26:33 -0700

    update

---
+++ src/Student9/Ex2/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footbook } from './Footbook'
+import './Header.scss'
+export const Header = () => {
+  return (
+    <div>
+        <div className="logo">
+            FootBook
+        </div>
+        <Footbook />
+    </div>
+  )
+}
