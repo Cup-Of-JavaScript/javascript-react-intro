e85f525e0fd38336280b6ca302a8d1bab3cb64ed
steven davis <sdavis@xpanxion.com>
2022-06-14 12:16:52 -0700

    update

---
+++ src/Student9/Ex2/Footbook.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footbook.scss'
+
+export const Footbook = () => {
+  return (
+    <div className="Footbook">
+        Home | FAQ | Support | Account
+    </div>
+  )
+}
