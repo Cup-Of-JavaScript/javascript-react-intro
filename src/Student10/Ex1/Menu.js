fbfb8fc3912ae5b418886ef4da32192d774d011a
Sherika Fayson <sfayson@xpanxion.com>
2022-06-14 10:57:03 -0400

    Ex. 1: Dollar Food Store

--- src/Student10/Ex1/Menu.js
+++ src/Student10/Ex1/Menu.js
@@ -1,40 +1,17 @@
 import React from 'react'
 import './Menu.scss'
+import { Drink } from './Drink'
+import { Food } from './Food'
+import { Dessert } from './Dessert'
+
 
 export const Menu = () => {
   return (
-    <div className='menu'>Menu
-    <div className='food'>
-        <h2>Food</h2>
-    <hr />
-    <ul>
-        <li>Hamburger ... $1.00</li>
-        <li>Cheeseburger... $1.00</li>
-        <li>Hot Dog... $1.00</li>
-        <li>Fries... $1.00</li>
-    </ul>
-     </div>
-
-     <div className='drink'>
-                <h2>Drink</h2>
-                <hr />
-                    <ul>
-                        <li>Coffee... $1.00</li>
-                        <li>Tea... $1.00</li>
-                        <li>Milk... $1.00</li>
-                        <li>Coke... $1.00</li>
-                    </ul>
-            </div>
-            <div className='dessert'> 
-                <h2>Dessert</h2>
-                <hr />
-                <ul>
-                    <li>Chocolate Pie... $1.00</li>
-                    <li>Apple Pie... $1.00</li>
-                    <li>Cannoli... $1.00</li>
-                </ul>
-            </div>
-    
+    <div className='menu'>
+      <h1 className="menu">Menu</h1>
+    <Food />
+    <Drink />
+    <Dessert />
     </div>
     
   )
