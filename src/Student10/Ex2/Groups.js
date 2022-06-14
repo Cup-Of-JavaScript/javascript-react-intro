5550c30deff10ac6b2ba1355b382224e1ef3cc49
Sherika Fayson <sfayson@xpanxion.com>
2022-06-14 11:50:00 -0400

    Ex. 2: FootBook

---
+++ src/Student10/Ex2/Groups.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Groups.scss'
+
+export const Groups = () => {
+  return (
+    <div className='groups'>
+        <h2>Groups</h2>
+        Likes: 12 Cats 
+        <br></br>
+        Likes: 2 Dogs 
+        <br></br>
+        Likes: 0 Good Neighbors
+    </div>
+  )
+}
---
+++ src/Student10/Ex2/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Posts.scss'
+
+export const Posts = () => {
+  return (
+    <div className="posts">
+        <h2>Posts</h2>
+        6/8/2022 Hello World!!!
+        <br></br>
+        6/9/2022 I think therefore I am.
+        <br></br>
+        6/12/2022 I need to retire.
+    </div>
+  )
+}
