12a8b95db65c8944701c9f1e89cc96fafa3cbf90
Kelsey Perkins <kperkins@xpanxion.com>
2022-06-15 08:06:05 -0700

    one line style solution

--- src/Student7/Ex3/Tweets.js
+++ src/Student7/Ex3/Tweets.js
@@ -1,21 +1,10 @@
 import React from 'react'
 import './Tweets.scss'
 
-const style1 = {
-    tweet1: {
-        backgorund: 'green',
-    }, 
-    tweet2: {
-        backgorund: 'yellow',
-    },
-    tweet2: {
-        backgorund: 'red',
-    },
-};
 
 export const Tweets = (props) => {
     return (
-        <div className="Tweets" style={style1.tweet1}>
+        <div className="Tweets" style={{backgroundColor: props.color}}>
            {props.message} 
         </div>
     )
