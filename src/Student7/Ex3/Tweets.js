bfce60564bf337fdcb6478eb6e6621cbb0a3dfe7
Kelsey Perkins <kperkins@xpanxion.com>
2022-06-15 08:01:59 -0700

    different style solution

--- src/Student7/Ex3/Tweets.js
+++ src/Student7/Ex3/Tweets.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Tweets.scss'
 
+const style1 = {
+    tweet1: {
+        backgorund: 'green',
+    }, 
+    tweet2: {
+        backgorund: 'yellow',
+    },
+    tweet2: {
+        backgorund: 'red',
+    },
+};
+
 export const Tweets = (props) => {
     return (
-        <div className="Tweets">
+        <div className="Tweets" style={style1.tweet1}>
            {props.message} 
         </div>
     )
