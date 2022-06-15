9d4d552eaf7ddd62cc4191e99338d635a275eb25
Antionette Van <avan@xpanxion.com>
2022-06-15 09:27:55 -0700

    update

---
+++ src/Student3/Ex2/Groups.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Groups.scss'
+
+export const Groups = () => {
+    return (
+        <div className="Groups">
+            <h2>Groups</h2>
+            <ul>
+                <li>Likes:  12 Cats</li>
+                <li>Likes: 2 Dogs</li>
+                <li>Likes: 0 Good Neighbors</li>
+            </ul>
+        </div>
+    )
+}
--- src/Student3/Ex2/Main.js
+++ src/Student3/Ex2/Main.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Header } from './Header'
+import { Posts } from './Posts'
+import { Groups } from './Groups'
+import { Footer } from './Footer'
 import './Main.css'
 
 export const Main = () => {
   return (
     <div>
         Hello I'm student id: 3.
+        <Header />
+        <Posts />
+        <Groups />
+        <Footer />
     </div>
   )
 }
