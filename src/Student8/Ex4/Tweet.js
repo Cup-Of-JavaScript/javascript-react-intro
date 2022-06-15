e8eea51782bf46f606f0b4e885c144265e48c391
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-06-15 16:10:14 -0400

    Ex4 Like Counter

---
+++ src/Student8/Ex4/Tweet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Tweet = (props) => {
+    return (
+        <div className='Tweet' style={{ backgroundColor: props.color }}>
+            {props.message}
+            <button onClick={() => props.onHandleClick(props.number)}>Like</button>
+        </div>
+    )
+}
+
--- src/Student8/Ex4/Tweets.js
+++ src/Student8/Ex4/Tweets.js
@@ -1,11 +1,20 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import './Tweets.scss'
+import { Tweet } from './Tweet'
 
 export const Tweets = (props) => {
+    let [counter, setCounter] = useState(0);
+    const onHandleClick = () => {
+        setCounter(++counter);
+    }
+
     return (
-        <div style={{backgroundColor: props.color}}>
-            {props.message}
+        <div className="Tweets">
+            Total Likes: {counter}
+            <Tweet message="It's payday!!!" color="green" onHandleClick={() => onHandleClick()} />
+            <Tweet message="I just bought a new car!" color="yellow" onHandleClick={() => onHandleClick()} />
+            <Tweet message="I just smashed my new car." color="red" onHandleClick={() => onHandleClick()} />
         </div>
     )
 }
