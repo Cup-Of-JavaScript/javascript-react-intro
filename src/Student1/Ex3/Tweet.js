d36e79fe87bcc56c404aee676c66a0a03aabc5ef
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-06-15 10:42:31 -0400

    finishing touches on tweet

--- src/Student1/Ex3/Tweet.js
+++ src/Student1/Ex3/Tweet.js
@@ -4,9 +4,8 @@ import './Tweet.scss'
 
 export const Tweet = (props) => {
     return (
-        <div className="Tweet">
+        <div className="Tweet" style = {{backgroundColor: props.color}}>
          {props.message}
-         {props.color}
         </div>
     )
 }
