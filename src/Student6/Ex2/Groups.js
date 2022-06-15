826527efabb806308df10c2585d637d4f19fc7af
Joel Sipin <jsipin@xpanxion.com>
2022-06-15 11:27:14 -0400

    Ex2: FootBook

---
+++ src/Student6/Ex2/Groups.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Groups.scss'
+
+export const Groups = () => {
+  return (
+    <div className="Groups">
+                <h2>Groups</h2>
+                <p>Likes:  12 Cats</p>
+                <p>Likes:  2 Dogs</p>
+                <p>Likes:  0 Good Neighbor</p>
+    </div>
+  )
+}
