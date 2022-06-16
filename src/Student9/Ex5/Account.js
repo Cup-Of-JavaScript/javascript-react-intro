27765d2f12c09c0c884bb00bd64590befa42a9e1
steven davis <sdavis@xpanxion.com>
2022-06-16 14:31:30 -0700

    update

---
+++ src/Student9/Ex5/Account.js
@@ -0,0 +1,48 @@
+import { useState} from 'react'
+import './Account.scss'
+
+
+export const Account = () => {
+let [show, setShow] = useState(false);
+const[buttonText, setButtonText] = useState('Show');
+
+return(
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Account Information</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Balance</td>
+          <td>$100,000</td>
+        </tr>
+        <tr>
+          <td>Interest</td>
+          <td>%5.0</td>
+        </tr>
+        <tr>
+          <td>Account Number</td>
+          <td> <div style={!show ? {} : {display: 'none'}} className = "Button">
+                  <button onClick={()=>{ 
+                    setShow (!show);
+                    setButtonText("Hide")
+                  }}>{buttonText}
+                  </button>
+                </div> 
+                <div style={show ? {}: {display: 'none'}}>11-1234
+                  <button onClick={()=> {setShow(!show); setButtonText ("Show")
+                  }}>{buttonText}
+                  </button>
+                </div>  
+          </td>       
+        </tr>
+      </tbody>
+    </table>
+  </div>
+)
+
+}
+
