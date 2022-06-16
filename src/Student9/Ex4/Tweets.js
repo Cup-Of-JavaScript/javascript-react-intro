b80b44a0147fc9deb779da4c2ae07353ae4b08b6
steven davis <sdavis@xpanxion.com>
2022-06-16 11:12:56 -0700

    update

---
+++ src/Student9/Ex4/Tweets.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Tweets.scss'
+import { Tweet } from './Tweet'
+import { useState } from 'react'
+
+
+export const Tweets = () => {
+    let [counter, setCounter] = useState(0);
+
+    const onClick = () => {
+        setCounter(++counter);
+
+    }
+    return (
+        <div className="Tweets">
+            <div>Total Likes:{counter}</div>
+            <Tweet message="It's payday!!!" color="green" onClick = {() => onClick ()}/>
+            <Tweet message="I just bought a new car!" color="yellow" onClick = {() => onClick ()}/>
+            <Tweet message="I just smashed my new car." color="red" onClick = {() => onClick ()}/>
+        </div>
+    )
+}
+
+   
+   
+   
+
