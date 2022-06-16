2087a593d6f21fa8ab6b00ec098cf007765eac47
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-06-16 13:50:22 -0400

    having trouble with show/hide button

---
+++ src/Student1/Ex5/Account.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState } from 'react';
+import "./Account.scss";
+import './ShowHide'
+import { ShowHide } from "./ShowHide";
+
+
+export const Account = () => {
+  let [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <table>
+        <thead><h1>Account Information</h1></thead>
+        <tbody>
+          <tr>
+            <td>Balance</td> &nbsp; <td>$100,000</td>
+          </tr>
+          <tr>
+            <td>Interest</td> &nbsp; <td>%5.0</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Account Number</td> &nbsp;
+            <span>
+              {
+                show &&
+                <div style={show ? {} : { display: 'none' }}>
+                  {show ? "11-1234" : ""}
+                </div>
+              }
+              &nbsp;
+            </span>
+            <td>
+              <td>
+                <button onClick={() => setShow(!show)}>Show</button>
+              </td>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  )
+}
