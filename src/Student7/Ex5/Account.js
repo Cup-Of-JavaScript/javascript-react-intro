26e6e0949d82885b0921738d497ed1d214ff8dc0
Kelsey Perkins <kperkins@xpanxion.com>
2022-06-16 20:49:01 -0700

    account component & table display

--- src/Student7/Ex5/Account.js
+++ src/Student7/Ex5/Account.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import './Account.scss'
+import { useState } from "react";
 
 
 export const Account = () => {
+  // let [show, setShow] = useState(false);
+  // let [buttonText, setButtonText] = useState(true);
   return (
-    <div>Account</div>
+    <div>
+      <table>
+        <tr>
+          <th>Account Information</th>
+        </tr>
+        <tr>
+          <td>Balance</td>
+          <td>$100,000</td>
+        </tr>
+        <tr>
+          <td>Interest</td>
+          <td>%5.0</td>
+        </tr>
+        <tr>
+          <td>Account Number</td>
+          <td>11-1234</td>
+        </tr>
+      </table>
+  </div>
   )
 }
